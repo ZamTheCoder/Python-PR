af538b02d4e31b4c49e35cd68a159dc54bd85414
ZamTheCoder <rudhav935@gmail.com>
2024-11-12 20:49:56 +0530

    first commit

---
+++ Import.py
@@ -0,0 +1,5 @@
+import keyword
+
+print(keyword.kwlist)
+
+print("False', 'None', 'True', 'and','del', 'global', 'if', 'import', 'in', 'is',not', 'or' not','while'" )
---
+++ Print.py
@@ -0,0 +1,23 @@
+#print ("Hello world")
+
+#print(9)
+
+#print("9")
+
+#print ("Hi\teveryone")
+
+#print("First Line \n Second Line")
+
+
+
+
+#name = "Rudhav"
+#grade = 7
+#school = "Maharishi Vidya Mandir"
+#print("My name is :", name, "\n My class is :", grade, "\n My School Name is :", school)
+
+
+
+
+
+
